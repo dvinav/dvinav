0bcae25e1aab1cd6340a22210f529ebcb7b05c89
Dvin Avanesian <109464831+DvinAvanesian@users.noreply.github.com>
2025-05-17 18:45:39 +0330

    fix wrong manifest name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const RootLayout: LC = async ({ children }) => {
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href={`/${locale}.json`} />
+        <link rel="manifest" href={`/manifest_${locale}.json`} />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="alternate" href={`https://${process.env.DOMAIN}/en/`} hrefLang="x-default" />
         <script
