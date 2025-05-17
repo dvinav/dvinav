9306a871efa1bf317898423f39ef2cb313b36a35
Dvin Avanesian <109464831+DvinAvanesian@users.noreply.github.com>
2025-05-17 16:36:03 +0330

    extend json ld

--- src/config/jsonld.ts
+++ src/config/jsonld.ts
@@ -1,19 +1,49 @@
-import { Person, WithContext } from 'schema-dts'
+import { Person, WebSite, WithContext } from 'schema-dts'
 
-const jsonLd: WithContext<Person> = {
+const me: WithContext<Person> = {
   '@context': 'https://schema.org',
   '@type': 'Person',
   name: 'Dvin Avanesian',
   url: 'https://dvinav.com',
-  jobTitle: 'Full Stack Developer and Linux Specialist',
+  jobTitle: 'Full Stack Developer',
+  email: 'dvinav@outlook.com',
+  telephone: '+989020092004',
+  identifier: 'dvin-avanesian-18-08-2004',
+  gender: 'https://schema.org/Male',
+  knowsLanguage: ['en', 'fa', 'hy-AM'],
+  knowsAbout: [
+    { '@type': 'Thing', name: 'React' },
+    { '@type': 'Thing', name: 'TypeScript' },
+    { '@type': 'Thing', name: 'MongoDB' },
+    { '@type': 'Thing', name: 'Redis' },
+    { '@type': 'Thing', name: 'Next.js' },
+    { '@type': 'Thing', name: 'MySQL' },
+    { '@type': 'Thing', name: 'Linux' },
+    { '@type': 'Thing', name: 'Node.js' },
+    { '@type': 'Thing', name: 'Express.js' },
+    { '@type': 'Thing', name: 'NestJS' },
+    { '@type': 'Thing', name: 'Hono.js' },
+    { '@type': 'Thing', name: 'C' },
+    { '@type': 'Thing', name: 'C++' },
+    { '@type': 'Thing', name: 'JavaScript' },
+    { '@type': 'Thing', name: 'Linux Administration' }
+  ],
   sameAs: [
     'https://github.com/dvinav',
     'https://linkedin.com/in/dvin-avanesian-0b4565283',
     'https://t.me/dvinav',
     'https://instagram.com/dvin.av',
-    'https://wa.me/989020092004',
-    'mailto:dvinav@outlook.com',
-    'tel:+989020092004'
+    'https://wa.me/989020092004'
   ]
 }
+
+const webSite: WithContext<WebSite> = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: 'Dvin Avanesian',
+  url: 'https://dvinav.com'
+}
+
+const jsonLd = [me, webSite]
+
 export default jsonLd
