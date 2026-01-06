bc6d6edddc985a32c9ca8358420f0d0262e77a00
Dvin Avanesian <109464831+DvinAvanesian@users.noreply.github.com>
2026-01-06 12:34:21 +0330

    add contactLinks

---
+++ src/config/contactLinks.ts
@@ -0,0 +1,25 @@
+import TelegramIcon from '@mui/icons-material/Telegram'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import MailRoundedIcon from '@mui/icons-material/MailRounded'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+
+const contactLinks = [
+  {
+    link: 'mailto:dvinav@outlook.com',
+    icon: MailRoundedIcon
+  },
+  {
+    link: 'https://github.com/dvinav',
+    icon: GitHubIcon
+  },
+  {
+    link: 'https://linkedin.com/in/dvinav',
+    icon: LinkedInIcon
+  },
+  {
+    link: 'https://t.me/dvinav',
+    icon: TelegramIcon
+  }
+]
+
+export default contactLinks
