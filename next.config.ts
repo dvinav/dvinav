f8ba98d08c43fac3198b82a05b7558df7675e762
Dvin Avanesian <109464831+DvinAvanesian@users.noreply.github.com>
2026-01-06 12:18:14 +0330

    remove next-rspack, next-compose-plugins, @next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import createNextIntlPlugin from 'next-intl/plugin'
-import withRspack from 'next-rspack'
-import withPlugins from 'next-compose-plugins'
 import type { NextConfig } from 'next'
-import bundleAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -11,10 +8,6 @@ const nextConfig: NextConfig = {
   }
 }
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true'
-})
-
 const withIntl = createNextIntlPlugin()
 
-export default withPlugins([withIntl, withRspack, withBundleAnalyzer], nextConfig)
+export default withIntl(nextConfig)
